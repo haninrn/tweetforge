22501b10c64adc5f2cbf375f7cfb8afcbfa5f502
hani <haninurain2409@gmail.com>
2024-05-24 14:52:26 +0800

    reg form two three and four, can connect to backend now

--- src/redux/Slices/RegisterSlice.ts
+++ src/redux/Slices/RegisterSlice.ts
@@ -1,5 +1,6 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import { Dob } from '../../features/register/utils/GlobalInterfaces';
+import axios from 'axios';
 
 interface RegisterSliceState {
     loading: boolean;
@@ -20,6 +21,13 @@ interface updatePayload {
     value:string | number | boolean;
 }
 
+interface RegisterUser {
+    firstName: string;
+    lastName: string;
+    email: string;
+    dob: string;
+}
+
 const initialState:RegisterSliceState = {
     loading: false,
     error: false,
@@ -37,6 +45,18 @@ const initialState:RegisterSliceState = {
     dobValid: false,
     step: 1
 }
+
+export const registerUser = createAsyncThunk(
+    'register/register', 
+    async (user:RegisterUser, thunkAPI) => {
+        try{
+            const req = await axios.post('http://localhost:8000/auth/register', user);
+            return await req.data;
+        } catch(e){
+            return thunkAPI.rejectWithValue(e);
+        }
+    }
+)
 
 export const RegisterSlice = createSlice({
     name:"register",
@@ -82,6 +102,23 @@ export const RegisterSlice = createSlice({
             }
         }
         
+    },
+    extraReducers: (builder) => {
+        builder.addCase(registerUser.pending, (state, action)=> {
+            state.loading = true;
+            return state;
+        });
+        builder.addCase(registerUser.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = false;
+            state.step++;
+            return state;
+        });
+        builder.addCase(registerUser.rejected, (state,action) => {
+            state.error = true;
+            state.loading = false;
+            return state;
+        })
     }
 });
 
